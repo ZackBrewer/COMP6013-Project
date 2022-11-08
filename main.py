5abac70cfbc11f065c4e73a98d87f539bc25d8d6
ZackBrewer <115952176+ZackBrewer@users.noreply.github.com>
2022-11-08 14:13:16 +0000

    Upload Main python file for project.

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'-----------------------------------------')
+    print(f'Welcome User, this is a project built by')
+    print(f'Zack Brewer. 19056696')
+    print(f'-----------------------------------------')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
